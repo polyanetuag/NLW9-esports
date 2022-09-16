8759012c0eb21e3ad0baf489d8c6ffd1abedca15
Polyane Tuag <pollyportugues@hotmail.com>
2022-09-16 18:34:22 -0300

    feat: create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,33 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+//ROTAS
+//listagem de games 
+app.get('/games', (request, response) => {
+    return response.json([])
+})
+
+//criar novo anúncio 
+app.post('/ads', (request, response) => {
+    return response.status(201).json([])
+})
+
+//listagem de anúncios por game
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id
+    
     return response.json([
         {id: 1, name: 'Anúncio 1'},
         {id: 2, name: 'Anúncio 2'},
         {id: 3, name: 'Anúncio 3'},
     ])
+})
+
+//buscar discord pelo Id do anúncio
+app.get('/ads/:id/discord', (request, response) => {
+    // const adId = request.params.id
+
+    return response.json([])
 })
 
 app.listen(3333);
